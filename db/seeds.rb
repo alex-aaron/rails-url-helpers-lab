d2cf7d32a3466d568f32f5b0214d782882af339b
Alex <alexfrancisaaron@gmail.com>
2021-02-17 12:52:13 -0600

    commit tree 7e23aeda460048d07a35014b4f8f4ab1e52dc215

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# student1 = Student.create(first_name: "Alex", last_name: "Aaron")
+# student2 = Student.create(first_name "Stephanie", last_name: "Cox", active:)
